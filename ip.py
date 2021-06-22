683bf7e9a5e60f0b8f1edaef9f6f76e6c65bb04e
Zain Mukati <45048274+epiczain1234@users.noreply.github.com>
2021-06-22 11:41:41 -0700

    Add files via upload

--- ip.py
+++ ip.py
@@ -5,38 +5,35 @@ from datetime import timedelta
 
 # Each time stamp is mapped to an list of tuples in format(virtual Server address, destination Ip)
 ips = dict()
-
-# iterates through the files in current directory
-num = 0
-for path in pathlib.Path(".").iterdir():
-    print(str(num) + '. ' + path.name)
-    num += 1
-numbers = input("Enter numbers of the files to analyze, seperated by spaces:")
-numbers = numbers.split(' ')
-chosen = set(numbers)
-
 index = 0
+pathnames = set()
 for path in pathlib.Path(".").iterdir():
     # if you change the name of this file from ip.py, must change the line below or this file will be processed as well and cause errors
-    if path.name != '.DS_Store' and path.name != 'ip.py' and str(index) in chosen:
+    if path.name != '.DS_Store' and path.name != 'ip.py':
         with open(path, 'r', encoding='windows-1252') as currFile:
             stamp = ''
             destination = ''
             virtualServer = ''
             for line in currFile:
-           #      print(line)
+                # print(line)
+                if 'Status' in line and 'STANDBY' in line:
+                    break
                 if 'Virtual' in line:
                     words = line.split()
                     virtualServer = words[2] 
                 if 'Destination' in line:
                     words = line.split()
-                    stamp = words[0][4:-5]
                     destination = words[-1]
+                if 'Bits Out' in line:
+                    words = line.split()
+                    stamp = words[0][4:-5]
                     if stamp in ips:
                         ips[stamp].append((virtualServer, destination, path.name))
                     else:
                         ips[stamp] = []
                         ips[stamp].append((virtualServer, destination, path.name))
+                    pathnames.add(path.name)
+                
         currFile.close()
     index += 1
 
@@ -47,8 +44,20 @@ keys = []
 for key in skeys:
     if key:
         keys.append(int(key))
-# print(keys)
 
+
+pathnames = list(pathnames)
+pathnames.sort()
+print("The active load balancers are:")
+for i in range(len(pathnames)):
+    print(str(i) + '. ' + pathnames[i][0:10])
+numbers = input("Enter numbers of the files to analyze, seperated by spaces:")
+numbers = numbers.split(' ')
+chosen = []
+for number in numbers:
+    chosen.append(int(number))
+
+keys.sort()
 # outputs dates based on those older than the input
 while True:
     n = input('Enter Minimum time Idled for servers (in days):')
@@ -58,9 +67,17 @@ while True:
     date = date.split('/')
     stamp = int(date[2] + date[0] + date[1])
     for key in keys:
-        if stamp >= key:
-            print('For date ' + str(key) + ' The Idled Virtual Sever address and Destination IPs are: =========================================================================================')
-            ipNd = ips[str(key)]
-            for virtualServer, destination, filename in ipNd:
-                print('File Name = ' + filename +  ' || Virtual Server = ' + virtualServer + ' || Destination IP = ' + destination)
+        for number in chosen:   
+            if stamp >= key:
+                # print('For date ' + str(key) + ' The Idled Virtual Sever address and Destination IPs are: =========================================================================================')
+                ipNd = ips[str(key)]
+                for virtualServer, destination, filename in ipNd:
+                    if filename == pathnames[number]:
+                        stampDays = str(key)
+                        stampDays = dt(int('20' + stampDays[0:2]), int(stampDays[2:4]), int(stampDays[4:]))
+                        days = dt.today() - stampDays
+                        # print(days)
+                        days = str(days).split(' ')
+                        days = days[0]
+                        print('File Names = ' + filename + ' || Idle Days = ' + str(days) +  ' || Virtual Server = ' + virtualServer + ' || Destination IP = ' + destination)
 
